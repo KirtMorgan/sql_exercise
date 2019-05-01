3b1f20f7f35263007bfe69c264a2d4f9f0e2e01b
Kirt Morgan <kirtmorgan@live.com>
2019-05-01 10:27:03 +0100

    Refactored the code and added inputs so the database now takes data and stores it

--- sql_connection.py
+++ sql_connection.py
@@ -9,8 +9,19 @@ docker_python_sql = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL SERVER};SERVE
 cursor = docker_python_sql.cursor()
 
 def add_order_sort(food, quantity, price, origin):
-    query = f"INSERT INTO food_order_table(name_of_food, quantity, price, origin) VALUES ('{food}','{quantity}','{price}','{origin}')"
-    nocount = """SET NOCOUNT ON;"""
-    cursor.execute(nocount + query)
+    try:
+        query = f"INSERT INTO food_order_table(name_of_food, quantity, price, origin) VALUES('{food}', {quantity}, {price}, '{origin}');"
+        cursor.execute(query)
+        docker_python_sql.commit()
+        print('The table has been updated, 1 row affected')
+    except:
+        print('There has been a error, record has not been committed')
 
-add_order_sort('nachos', 1, 10.00, 'america')
+def add_spartan(name_of_student, phone_number, course, trainer):
+    try:
+            query = f"INSERT INTO sparta_table(name_of_student, phone_number, course, trainer) VALUES('{name_of_student}', {phone_number}, '{course}', '{trainer}');"
+            cursor.execute(query)
+            docker_python_sql.commit()
+            print('The table has been updated, 1 row affected')
+    except:
+        print('There has been a error, record has not been committed')
